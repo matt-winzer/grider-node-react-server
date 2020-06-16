584f96763952b2fec83b9649bb3b02060a1d674a
Matt Winzer <matt.winzer@gmail.com>
2020-06-16 17:09:25 -0600

    Configure initial google oauth strategy

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const googleClientID = process.env.GOOGLE_CLIENT_ID;
+const googleClientSecret = process.env.GOOGLE_CLIENT_SECRET;
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: googleClientID,
+      clientSecret: googleClientSecret,
+      callbackURL: '/auth/google/callback',
+    },
+    (accessToken) => {
+      console.log(accessToken);
+    }
+  )
+);
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
