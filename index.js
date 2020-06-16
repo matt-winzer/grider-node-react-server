e99650f6828dbacbfa3e65b4be088f8333e67051
Matt Winzer <matt.winzer@gmail.com>
2020-06-16 17:07:25 -0600

    Add passport, passport-google-oauth20, dotenv dependencies

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const express = require('express');
+const passport = require('passport');
+
+const PORT = process.env.PORT || 5000;
 const app = express();
-
-const PORT = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  res.json({ hi: 'there' });
-});
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
