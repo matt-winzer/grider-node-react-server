1eb1db7b08da697a21a6144fcab2c384e0b75e84
Matt Winzer <matt.winzer@gmail.com>
2020-06-25 16:13:08 -0600

    Configure cookie-session and passport middleware

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const app = express();
 
 // environment variables
 const PORT = process.env.PORT || 5000;
-const { MONGO_URI } = process.env;
+const { MONGO_URI, COOKIE_KEY } = process.env;
 
 // routers
 const authRoutes = require('./routes/authRoutes');
@@ -21,6 +23,16 @@ mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+// middleware
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 40 * 1000,
+    keys: [COOKIE_KEY],
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 // routes
 app.use('/auth/google', authRoutes);
